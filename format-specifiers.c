7209e05a7cd686d31e2736ca1b60dec11cec4b46
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-03-19 07:35:25 +0100

    Learn format specifiers

--- format-specifiers.c
+++ format-specifiers.c
@@ -17,9 +17,9 @@ int main (void)
     float item2 = 10.00;
     float item3 = 100.99;
 
-    printf("Item 1: $%.2f\n", item1);
-    printf("Item 2: $%.2f\n", item2);
-    printf("Item 3: $%.2f\n", item3);
+    printf("Item 1, 2 and 3: $%-8.2f $%-8.2f $%-8.2f\n", item1, item2, item3);
+    printf("Item 2: $%8.2f\n", item2);
+    printf("Item 3: $%8.2f\n", item3);
     
     return 0;
 
